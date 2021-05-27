dbc85659bd4197a693c3023f53894c1b0f0dc47f
UzairMK <uzairmkhan584@gmail.com>
2021-05-27 12:29:15 +0100

    created and set up test project

---
+++ Tech_Test_Raindrops/Raindrops_Tests/Raindrop_Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using RaindropsLib;
+
+namespace Raindrops_Tests
+{
+    public class Rainsdrop_Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
