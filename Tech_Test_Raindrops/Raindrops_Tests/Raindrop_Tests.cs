88bdd4e916e23c369450ba290ae2fb7f7fecc8fc
UzairMK <uzairmkhan584@gmail.com>
2021-05-27 12:34:42 +0100

    created and passed not divisible by 3,5,or 7 test

--- Tech_Test_Raindrops/Raindrops_Tests/Raindrop_Tests.cs
+++ Tech_Test_Raindrops/Raindrops_Tests/Raindrop_Tests.cs
@@ -5,10 +5,15 @@ namespace Raindrops_Tests
 {
     public class Rainsdrop_Tests
     {
-        [Test]
-        public void Test1()
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(4, "4")]
+        [TestCase(8, "8")]
+        [TestCase(11, "11")]
+        public void NotDivisibleBy3_5_Or7(int input, string expected)
         {
-            Assert.Pass();
+            string actual = Raindrop.Input(input);
+            Assert.AreEqual(expected,actual);
         }
     }
 }
