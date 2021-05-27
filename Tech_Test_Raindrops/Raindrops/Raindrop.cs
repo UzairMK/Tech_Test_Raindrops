7b9b3ccf989e82ef84c79a2da146867990696a9e
UzairMK <uzairmkhan584@gmail.com>
2021-05-27 12:24:32 +0100

    created raindrop class libarary with the required function

---
+++ Tech_Test_Raindrops/Raindrops/Raindrop.cs
@@ -0,0 +1,20 @@
+﻿namespace RaindropsLib
+{
+    public static class Raindrop
+    {
+        public static string Input(int number)
+        {
+            string output = "";
+            if (number % 3 == 0)
+                output += "Pling";
+            if (number % 5 == 0)
+                output += "Plang";
+            if (number % 7 == 0)
+                output += "Plong";
+            if (output == "")
+                return number.ToString();
+            else
+                return output;
+        }
+    }
+}
